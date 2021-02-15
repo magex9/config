96fbfd53c923bbf9bde50b4a01db02e3bd58ffae
Scott <scottfinlay273@outlook.com>
2021-02-15 07:26:19 -0500

    Added a list of file i use to chocolately

---
+++ chocolatey.bat
@@ -0,0 +1,23 @@
+
+cinst -y chocolateygui
+cinst -y 7zip.install
+cinst -y qdir
+cinst -y windirstat
+cinst -y googlechrome
+cinst -y firefox
+cinst -y notepadplusplus.install
+cinst -y conemu
+cinst -y microsoft-teams
+cinst -y slack
+
+cinst -y nginx
+cinst -y adoptopenjdk11
+cinst -y maven
+cinst -y nodejs-lts
+cinst -y cloudfoundry-cli
+cinst -y git.install
+cinst -y gitextensions
+cinst -y postman
+cinst -y intellijidea-ultimate
+cinst -y jetbrainstoolbox
+cinst -y webstorm
